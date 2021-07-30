a7891d8836834de2c772bd47c9b9caacb98492eb
kalpak44 <avgustares@gmail.com>
2021-07-30 19:08:12 +0300

    docker-compose experiment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-echo -e "Host $VPS_HOST\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-echo $VPS_KEY | base64 --decode > ~/.ssh/travis_rsa
+echo -e "Host $VPS_HOST\n\tStrictHostKeyChecking no\n" >>~/.ssh/config
+echo $VPS_KEY | base64 --decode >~/.ssh/travis_rsa
 chmod 600 ~/.ssh/travis_rsa
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/travis_rsa
-ssh -i ~/.ssh/travis_rsa -o UserKnownHostsFile=/dev/null  $VPS_USERNAME@$VPS_HOST "docker version"
+ssh -i ~/.ssh/travis_rsa -o UserKnownHostsFile=/dev/null $VPS_USERNAME@$VPS_HOST "docker version && docker-compose version"
 scp docker-compose.yml $VPS_USERNAME@$VPS_HOST:/root/docker-compose
+ssh -i ~/.ssh/travis_rsa -o UserKnownHostsFile=/dev/null $VPS_USERNAME@$VPS_HOST "export TAG_NAME=latest && export BOT_TOKEN=${BOT_TOKEN} && export BOT_USERNAME=${BOT_USERNAME} && export PAYMENTS_STRIPE_KEY_PUBLIC=${PAYMENTS_STRIPE_KEY_PUBLIC} && export PAYMENTS_STRIPE_KEY_SECRET=${PAYMENTS_STRIPE_KEY_SECRET} && docker-compose up"
