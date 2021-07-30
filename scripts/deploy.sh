4402a5724a6e2a8eb971b64ef19f457f4965bd05
kalpak44 <avgustares@gmail.com>
2021-07-30 19:58:48 +0300

    docker-compose experiment redeploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,4 +6,4 @@ eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/travis_rsa
 ssh -i ~/.ssh/travis_rsa -o UserKnownHostsFile=/dev/null $VPS_USERNAME@$VPS_HOST "docker version && docker-compose version"
 scp docker-compose.yml $VPS_USERNAME@$VPS_HOST:/root/docker-compose
-ssh -i ~/.ssh/travis_rsa $VPS_USERNAME@$VPS_HOST "cd /root/docker-compose && export TAG_NAME=${$TRAVIS_BRANCH} && export BOT_TOKEN=${BOT_TOKEN} && export BOT_USERNAME=${BOT_USERNAME} && export PAYMENTS_STRIPE_KEY_PUBLIC=${PAYMENTS_STRIPE_KEY_PUBLIC} && export PAYMENTS_STRIPE_KEY_SECRET=${PAYMENTS_STRIPE_KEY_SECRET} && docker-compose up --detach --force-recreate"
+ssh -i ~/.ssh/travis_rsa $VPS_USERNAME@$VPS_HOST "cd /root/docker-compose && export TAG_NAME=${TRAVIS_BRANCH} && export BOT_TOKEN=${BOT_TOKEN} && export BOT_USERNAME=${BOT_USERNAME} && export PAYMENTS_STRIPE_KEY_PUBLIC=${PAYMENTS_STRIPE_KEY_PUBLIC} && export PAYMENTS_STRIPE_KEY_SECRET=${PAYMENTS_STRIPE_KEY_SECRET} && docker-compose up --detach --force-recreate"
