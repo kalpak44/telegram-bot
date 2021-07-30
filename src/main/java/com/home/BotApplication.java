e61010b53552f29fbec83a5c9dba0c31fc7d8a21
kalpak44 <avgustares@gmail.com>
2021-07-30 19:28:00 +0300

    docker-compose experiment redeploy

--- src/main/java/com/home/BotApplication.java
+++ src/main/java/com/home/BotApplication.java
@@ -1,11 +1,16 @@
 package com.home;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BotApplication {
+    private static final Logger logger = LoggerFactory.getLogger(BotApplication.class);
+
     public static void main(String[] args) {
+        logger.info("started");
         SpringApplication.run(BotApplication.class, args);
     }
 }
